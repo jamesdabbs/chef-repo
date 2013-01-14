2988d77c7a87257adfca7d805d010e2d2a57cf16
James Dabbs <jdabbs@emcien.com>
2013-01-13 23:07:32 -0500

    Add `jdabbs` cookbook and basic .bashrc recipe.

---
+++ cookbooks/jdabbs/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: jdabbs
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+template "#{ENV['HOME']}/.bashrc" do
+  source "bashrc.erb"
+  mode   "0644"
+end
