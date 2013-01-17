64ee974eb4484950924df8d5497fc79b12560af4
James Dabbs <jdabbs@emcien.com>
2013-01-16 21:41:59 -0500

    Add transmission watch dir.

--- roles/server.rb
+++ roles/server.rb
@@ -1,4 +1,7 @@
 name 'server'
 description 'Full bespoke configuration for a home server'
-recipes = %w{ chef-client jdabbs }
+
+recipes = %w{ chef-client jdabbs transmission }
 run_list recipes.map { |r| "recipe[#{r}]"}
+
+default_attributes 'transmission' => { 'watch_dir_enabled' => 'true' }
