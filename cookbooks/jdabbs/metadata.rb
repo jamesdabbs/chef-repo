4dfe1c53029e280ef2f2257137bccd2291b4f6b9
James Dabbs <jdabbs@emcien.com>
2013-01-19 12:09:39 -0500

    Update metadata

--- cookbooks/jdabbs/metadata.rb
+++ cookbooks/jdabbs/metadata.rb
@@ -1,6 +1,8 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       'James Dabbs'
+maintainer_email 'jamesdabbs@gmail.com'
 license          "All rights reserved"
 description      "Installs/Configures jdabbs"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+
+depends 'python'
